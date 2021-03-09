19f13743b6aa03dbb888a7ee33403648c19c39f6
Bruno Martins <brunorodrigues929@gmail.com>
2021-03-09 14:43:19 -0300

    configured

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hellow!' })
-})
+app.get('/', (request, response) => response.json({ message: 'Hellow!' }));
 
 app.listen(3333, () => {
-    console.log("Servidor no ar! Port:3333");
-})
+  console.log('Servidor no ar! Port:3333');
+});
